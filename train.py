8c7c6b2489e8e39b1bb3e86917516339ef3090f2
WEN SHANG <52658829+swsw123123@users.noreply.github.com>
2019-11-26 10:07:22 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,17 +1,3 @@
-"""
-TRAIN GANOMALY
-
-. Example: Run the following command from the terminal.
-    run train.py                             \
-        --model ganomaly                        \
-        --dataset UCSD_Anomaly_Dataset/UCSDped1 \
-        --batchsize 32                          \
-        --isize 256                         \
-        --nz 512                                \
-        --ngf 64                               \
-        --ndf 64
-"""
-
 
 ##
 # LIBRARIES
